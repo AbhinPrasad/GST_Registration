164c0097e2bd5577a32cb28f77652a4c3be98ef8
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-02-11 17:33:45 +0530

    order created - razorpay

--- ecfile_server/controllers/paymentController.js
+++ ecfile_server/controllers/paymentController.js
@@ -1,6 +1,27 @@
-import User from "../models/userModel.js"
+import User from "../models/userModel.js";
+import Razorpay from "razorpay";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const initiatePayment = async (req,res)=>{
-    console.log(req.body);
-    res.send("helllo")
-}
+console.log(process.env.KEY_ID);
+const instance = new Razorpay({
+	key_id: process.env.KEY_ID,
+	key_secret: process.env.KEY_SECRET
+});
+export const initiatePayment = async (req, res) => {
+	const { companyName, phone, email, aadhaar, pan, option } = req.body;
+
+	const service = option.name;
+	const amount = option.price;
+
+	// ORDER CREATION
+    var options = {
+        amount: amount ,  // amount in the smallest currency unit
+        currency: "INR",
+        receipt: "order_rcptid_11"
+      };
+      instance.orders.create(options, function(err, order) {
+        console.log(order);
+        res.json(order)
+      });
+};
