86c8d52e1cc084c2a1e7f5a57b910d719577cb75
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-02-12 17:21:59 +0530

    admin model created

---
+++ ecfile_server/models/adminModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const adminSchema = mongoose.Schema({
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;
